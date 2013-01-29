456e10e4e932476d43c66fe327c2219bb17e39a8
José Padilla <jpadilla@webapplicate.com>
2013-01-29 13:59:42 -0400

    Latest changes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*
 
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 readme = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(readme).read()
@@ -9,10 +9,12 @@ long_description = open(readme).read()
 setup(
     name='python-postmark-inbound',
     version='0.1',
-    packages=['postmark'],
+    packages=find_packages(),
     author='José Padilla',
     author_email='jpadilla@webapplicate.com',
     description='Python wrapper for Postmark Inbound',
     long_description=long_description,
+    license='MIT License',
     url='https://github.com/jpadilla/postmark-inbound-python',
+    download_url='https://github.com/jpadilla/postmark-inbound-python/tarball/master',
 )
