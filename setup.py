29f7fb8bdb0ed0c8424aa0a83928c84041ebc1e8
José Padilla <jpadilla@webapplicate.com>
2014-08-22 11:03:55 -0400

    Include publish helper

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 # -*- coding: utf-8 -*
-
 import os
 from setuptools import setup, find_packages
 
+
 readme = os.path.join(os.path.dirname(__file__), 'README.md')
 long_description = open(readme).read()
+
+
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    os.system("python setup.py bdist_wheel upload")
+    print("You probably want to also tag the version now:")
+    print("  git tag -a {0} -m 'version {0}'".format(version))
+    print("  git push --tags")
+    sys.exit()
 
 setup(
     name='python-postmark-inbound',
